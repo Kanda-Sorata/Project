36fa96e5982eccdefb7e0e991b9b1c657402b387
Kanda-Sorata <corentin.rousseaux.01@student.henallux.be>
2019-02-27 10:50:09 +0100

    1

---
+++ Serie_4/src/Principal/Paroi.java
@@ -0,0 +1,11 @@
+package Principal;
+
+import java.awt.*;
+
+public class Paroi {
+    private Rectangle rectangle;
+
+    public Paroi(int x0, int y0, int height, int width){ //height hauteur width largeur
+
+    }
+}
