40dba4e2896be791324c59e1ea34d97cfe7ad954
Kanda-Sorata <corentin.rousseaux.01@student.henallux.be>
2019-04-13 16:14:44 +0200

    delete unneccessary directories

--- Serie_2/src/Exception/TooMuchActivities.java
+++
@@ -1,8 +0,0 @@
-package Exception;
-
-public class TooMuchActivities extends Exception{
-
-    public String getMessage(){
-        return "L'activitée n'a pas été ajoutée, la liste des cours est complète.";
-    }
-}
